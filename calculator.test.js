dc8f9f3fd478a3945c975e484af9c63632c9b4f3
Shyam Vachhani <127606836+ShyamVachhani@users.noreply.github.com>
2025-07-04 21:23:35 +0530

    ✅ Test: returns sum of multiple numbers

--- calculator.test.js
+++ calculator.test.js
@@ -3,12 +3,15 @@ const { add } = require("./calculator");
 test("empty string returns 0", () => {
   expect(add("")).toBe(0);
 });
+
 test("single number returns its value", () => {
   expect(add("1")).toBe(1);
 });
+
 test('returns sum of two numbers', () => {
   expect(add("1,5")).toBe(6);
 });
+
 test('returns sum of multiple numbers', () => {
-  expect(add("1,2,3,4")).toBe(10);
+  expect(add("1,2,3,4,5")).toBe(15);
 });
