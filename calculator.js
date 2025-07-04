f67cd13fb77b5e2d50483b4f12783de64d7b7b77
Shyam Vachhani <127606836+ShyamVachhani@users.noreply.github.com>
2025-07-04 21:21:37 +0530

    ✅ Test: returns sum of two numbers

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
 function add(numbers) {
   if (numbers === "") return 0;
-  return parseInt(numbers);
+  const nums = numbers.split(",").map(Number);
+  return nums.reduce((a, b) => a + b, 0);
 }
 
 module.exports = { add };
