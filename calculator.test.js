0b16097e5fd3a85a3346cab2fb27b3fda8e2f666
Shyam Vachhani <127606836+ShyamVachhani@users.noreply.github.com>
2025-07-04 21:08:47 +0530

    ✅ Test: returns number itself for single input

--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,6 @@ const { add } = require("./calculator");
 test("empty string returns 0", () => {
   expect(add("")).toBe(0);
 });
+test("single number returns its value", () => {
+  expect(add("1")).toBe(1);
+});
